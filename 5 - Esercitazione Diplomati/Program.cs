19235e20cc27a08983622fc23e02952d18fd94a0
Nesi.20118 <Nesi.20118@LABI2-13PC22>
2023-12-04 09:57:26 +0100

    Aggiungere i file di progetto.

---
+++ 5 - Esercitazione Diplomati/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___Esercitazione_Diplomati
+{
+    class Diplomati
+    {
+        public string[] Nomivecchi { get; set; }
+        public int[] Votivecchi { get; set; }
+
+        public Diplomati(string[] nomivecchi,int[] votivecchi)
+        {
+            Nomivecchi = nomivecchi;
+            Votivecchi = votivecchi;
+        }
+        public virtual string visualizzavecchi()
+        {
+            Console.WriteLine("Vecchi studenti: " +  Nomivecchi + " Voto: " + Votivecchi);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
