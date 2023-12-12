d25d804d121ed85d26f7ae3dfb92309d098ee837
Nesi.20118 <Nesi.20118@LABI2-13PC22>
2023-12-12 10:34:50 +0100

    mod

--- 5 - Esercitazione Diplomati/Program.cs
+++ 5 - Esercitazione Diplomati/Program.cs
@@ -8,23 +8,41 @@ namespace _5___Esercitazione_Diplomati
 {
     class Diplomati
     {
-        public string[] Nomivecchi { get; set; }
-        public int[] Votivecchi { get; set; }
+        public string[] Nomi { get; set; }
+        public int[] Voti{ get; set; }
 
-        public Diplomati(string[] nomivecchi,int[] votivecchi)
+        public Diplomati(string[] nomi,int[] voti)
         {
-            Nomivecchi = nomivecchi;
-            Votivecchi = votivecchi;
+            Nomi = nomi;
+            Voti = voti;
         }
-        public virtual string visualizzavecchi()
+        public virtual string visualizzadiplomati()
         {
-            Console.WriteLine("Vecchi studenti: " +  Nomivecchi + " Voto: " + Votivecchi);
+            return $"Vecchi studenti: " +  Nomi + " Voto: " + Voti;
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserire numero Diplomati: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Inserire nome Diplomato e voto: ");
+            string[] n = new string[num];
+            int[] v = new int[num];
+            for (int i=0; i < num; i++)
+            {
+                Console.Write("Nome: ");
+                n[i] = Console.ReadLine();
+                Console.Write("Voto: ");
+                v[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            Diplomati diplomati = new Diplomati(n, v);
+            Console.WriteLine(diplomati.visualizzadiplomati());
+            Console.ReadLine();
+            
         }
     }
 }
